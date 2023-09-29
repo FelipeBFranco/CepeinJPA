fec366f71d3b43e6cf4a03548481fa4d89f4dd03
marco <1811420373@fema.edu.br>
2023-09-29 09:39:07 -0300

    Projeto Inicial com Dependências necessárias e banco configurado

---
+++ src/main/java/cepein/mapeamento/CepeinApplication.java
@@ -0,0 +1,13 @@
+package cepein.mapeamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepeinApplication.class, args);
+	}
+
+}
