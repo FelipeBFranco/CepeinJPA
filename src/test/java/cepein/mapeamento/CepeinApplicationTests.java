939f12825c7430fd44042036af397198fa4418e0
Felipe Franco <2311600613@fema.edu.br>
2023-10-19 09:20:14 -0300

    Relacionamentos e metodos feitos.

--- src/test/java/cepein/mapeamento/CepeinApplicationTests.java
+++ src/test/java/cepein/mapeamento/CepeinApplicationTests.java
@@ -1,13 +1,11 @@
 package cepein.mapeamento;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CepeinApplicationTests {
+public class CepeinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
+
