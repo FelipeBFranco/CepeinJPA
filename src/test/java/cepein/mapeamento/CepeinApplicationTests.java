617c534a84d0965b57b7bf1aa416f41acf42b6e5
Felipe Franco <2311600613@fema.edu.br>
2023-10-26 08:54:23 -0300

    Metodo de teste feito.

--- src/test/java/cepein/mapeamento/CepeinApplicationTests.java
+++ src/test/java/cepein/mapeamento/CepeinApplicationTests.java
@@ -1,10 +1,36 @@
 package cepein.mapeamento;
 
 
+import cepein.mapeamento.pessoa.model.Pessoa;
+import cepein.mapeamento.pessoa.repository.PessoaRepository;
+import cepein.mapeamento.pessoa.service.PessoaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CepeinApplicationTests {
+
+    @Test
+    public void deveSalvarPessoa() {
+
+        // Cria uma pessoa
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("MarcolaDoBCC");
+
+        PessoaRepository mockRepositorio = Mockito.mock(PessoaRepository.class);
+
+
+        Mockito.when(mockRepositorio.save(pessoa)).thenReturn(pessoa);
+
+
+        PessoaService service = new PessoaService(mockRepositorio);
+
+        Pessoa pessoaSalva = service.salvar(pessoa);
+
+        //Assertions.assertNull(pessoaSalva);
+    }
 
 
 }
