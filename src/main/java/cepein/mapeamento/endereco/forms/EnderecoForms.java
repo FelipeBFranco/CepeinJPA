6483cbbd68094ea63523fa538f452a6675edb285
Erick Sikina Duarte <2211550521@fema.edu.br>
2024-02-07 10:53:25 -0300

    corrigir endereco

--- src/main/java/cepein/mapeamento/endereco/forms/EnderecoForms.java
+++ src/main/java/cepein/mapeamento/endereco/forms/EnderecoForms.java
@@ -13,10 +13,6 @@ public class EnderecoForms {
     @Size(max = 250, message = "O campo 'descriçao' pode ter no máximo {MAX} caracteres!")
     private String uuid;
 
-
-
-
-
     public Endereco convet(Endereco endereco){
         endereco.setUuid(this.getUuid());
         return endereco;
--- src/main/java/cepein/mapeamento/endereco/service/EnderecoService.java
+++ src/main/java/cepein/mapeamento/endereco/service/EnderecoService.java
@@ -4,6 +4,8 @@ import cepein.mapeamento.endereco.dto.EnderecoDto;
 import cepein.mapeamento.endereco.forms.EnderecoForms;
 import cepein.mapeamento.endereco.model.Endereco;
 import cepein.mapeamento.endereco.repository.EnderecoRepository;
+import exception.ObjectNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,29 +21,39 @@ public class EnderecoService {
         this.enderecoRepository = enderecoRepository;
     }
 
+    private Endereco buscarEndereco(Long idEndereco){
+        return this.enderecoRepository.findById(idEndereco)
+                .orElseThrow(() -> new ObjectNotFoundException("Endereço não encontrado"));
+    }
+
     public ResponseEntity<List<EnderecoDto>> listarEnderecos(){
         List<Endereco> enderecoList = this.enderecoRepository.findAll();
         List<EnderecoDto> enderecoDtoList = EnderecoDto.convet(enderecoList);
         return ResponseEntity.ok(enderecoDtoList);
     }
+
     public ResponseEntity<EnderecoDto> procurarEndereco(Long idEndereco){
-        Endereco endereco = this.enderecoRepository.findById(idEndereco)
-                .orElseThrow(()-> new RuntimeException("endereco não encontrado"));
+        Endereco endereco = this.buscarEndereco(idEndereco);
         EnderecoDto enderecoDto = new EnderecoDto(endereco);
         return ResponseEntity.ok(enderecoDto);
     }
+
+    @Transactional
     public ResponseEntity<Void> cadastrarEndereco(EnderecoForms enderecoForms){
         Endereco endereco = enderecoForms.convet(new Endereco());
         this.enderecoRepository.save(endereco);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @Transactional
     public ResponseEntity<Void> alterarEndereco(Long idEndereco,EnderecoForms enderecoForms){
-        Endereco endereco = this.enderecoRepository.findById(idEndereco)
-                .orElseThrow(()-> new RuntimeException("endereco não encontrado"));
+        Endereco endereco = this.buscarEndereco(idEndereco);
         Endereco enderecoAlterado = enderecoForms.convet(endereco);
         this.enderecoRepository.save(enderecoAlterado);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @Transactional
     public ResponseEntity<Void> deletarEndereco(Long idEndereco){
         this.enderecoRepository.deleteById(idEndereco);
         return ResponseEntity.status(HttpStatus.CREATED).build();
