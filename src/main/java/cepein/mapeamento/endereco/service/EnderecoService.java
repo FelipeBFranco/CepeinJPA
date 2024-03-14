db7d3365675768cb348e264cf1175f4023536fd3
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-03-14 09:40:22 -0300

    refactor: tirar a responsabilidade de retornar a resposta Http do service de endereco

--- src/main/java/cepein/mapeamento/endereco/service/EnderecoService.java
+++ src/main/java/cepein/mapeamento/endereco/service/EnderecoService.java
@@ -5,9 +5,10 @@ import cepein.mapeamento.endereco.forms.EnderecoForms;
 import cepein.mapeamento.endereco.model.Endereco;
 import cepein.mapeamento.endereco.repository.EnderecoRepository;
 import exception.ObjectNotFoundException;
+import exception.PessoaExistException;
 import jakarta.transaction.Transactional;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,45 +17,46 @@ import java.util.List;
 public class EnderecoService {
     private final EnderecoRepository enderecoRepository;
 
+    @Autowired
     public EnderecoService(EnderecoRepository enderecoRepository) {
         this.enderecoRepository = enderecoRepository;
     }
 
-    private Endereco buscarEndereco(Long idEndereco){
+    public Endereco buscarEndereco(Long idEndereco){
         return this.enderecoRepository.findById(idEndereco)
                 .orElseThrow(() -> new ObjectNotFoundException("Endereço não encontrado"));
     }
 
-    public ResponseEntity<List<EnderecoDto>> listarEnderecos(){
+    public List<EnderecoDto> listarEnderecos(){
         List<Endereco> enderecoList = this.enderecoRepository.findAll();
         List<EnderecoDto> enderecoDtoList = EnderecoDto.converter(enderecoList);
-        return ResponseEntity.ok(enderecoDtoList);
+        return enderecoDtoList;
     }
 
-    public ResponseEntity<EnderecoDto> procurarEndereco(Long idEndereco){
+    public EnderecoDto procurarEndereco(Long idEndereco){
         Endereco endereco = this.buscarEndereco(idEndereco);
         EnderecoDto enderecoDto = new EnderecoDto(endereco);
-        return ResponseEntity.ok(enderecoDto);
+        return enderecoDto;
     }
 
     @Transactional
-    public ResponseEntity<Void> cadastrarEndereco(EnderecoForms enderecoForms){
+    public Void cadastrarEndereco(EnderecoForms enderecoForms){
+
         Endereco endereco = enderecoForms.converter(new Endereco());
         this.enderecoRepository.save(endereco);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return null;
     }
-
     @Transactional
-    public ResponseEntity<Void> alterarEndereco(Long idEndereco,EnderecoForms enderecoForms){
+    public Void alterarEndereco(Long idEndereco,EnderecoForms enderecoForms){
         Endereco endereco = this.buscarEndereco(idEndereco);
         Endereco enderecoAlterado = enderecoForms.converter(endereco);
         this.enderecoRepository.save(enderecoAlterado);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return null;
     }
 
     @Transactional
     public ResponseEntity<Void> deletarEndereco(Long idEndereco){
         this.enderecoRepository.deleteById(idEndereco);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return null;
     }
 }
