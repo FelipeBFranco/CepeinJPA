5c4ef3cb7b8c932f854976c48ccc81336664df0c
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-04-15 09:58:59 -0300

    teste: criar teste de pedido

---
+++ src/test/java/cepein/mapeamento/service/PedidoServiceTest.java
@@ -0,0 +1,32 @@
+package cepein.mapeamento.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoServiceTest {
+
+    @Test
+    void buscarPedido() {
+    }
+
+    @Test
+    void listarPedidos() {
+    }
+
+    @Test
+    void procurarPedido() {
+    }
+
+    @Test
+    void cadastrarPedido() {
+    }
+
+    @Test
+    void atualizarPedidoExistente() {
+    }
+
+    @Test
+    void deletarPedido() {
+    }
+}
