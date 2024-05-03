69fe62f777d09d4d2e65bad7edc71b7841ad1d79
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-03 13:27:59 -0300

    fix: deletar classes inutilizadas de produto

--- src/main/java/cepein/mapeamento/infra/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package cepein.mapeamento.infra.repository;
-
-import cepein.mapeamento.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
