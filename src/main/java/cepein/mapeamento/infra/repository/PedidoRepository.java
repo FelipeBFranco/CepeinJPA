82e467f61e758225307e19010662755b2f3bd149
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-03 11:51:17 -0300

    fix: deletar classes inutilizadas de pedido

--- src/main/java/cepein/mapeamento/infra/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package cepein.mapeamento.infra.repository;
-
-import cepein.mapeamento.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
