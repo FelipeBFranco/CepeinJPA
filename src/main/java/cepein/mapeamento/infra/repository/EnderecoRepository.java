67bdd40f8651c0ed9e711f1cec7372f4a46a6e86
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-03 11:34:54 -0300

    fix: deletar classes inutilizadas de endereço

--- src/main/java/cepein/mapeamento/infra/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package cepein.mapeamento.infra.repository;
-
-import cepein.mapeamento.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
-}
