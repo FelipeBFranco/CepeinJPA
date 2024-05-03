5d540d7d101140bff0b4d3f43b8c6c1af57ae698
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-03 11:30:15 -0300

    fix: deletar classes inutilizadas de curso

--- src/main/java/cepein/mapeamento/infra/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package cepein.mapeamento.infra.repository;
-
-import cepein.mapeamento.model.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
