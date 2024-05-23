a2a388988fc35fa16be8c053d091c9214d463e9e
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 10:20:50 -0300

    refactor: implementar interface use case em produto use case

--- src/main/java/cepein/mapeamento/app/usecases/produto/AtualizarProdutoUseCase.java
+++ src/main/java/cepein/mapeamento/app/usecases/produto/AtualizarProdutoUseCase.java
@@ -1,15 +1,19 @@
 package cepein.mapeamento.app.usecases.produto;
 
-import cepein.mapeamento.acore.domain.models.Produto;
+import cepein.mapeamento.acore.domain.models.produto.ProdutoQuery;
 import cepein.mapeamento.app.gateways.ProdutoGateway;
+import cepein.mapeamento.infra.adapters.http.forms.ProdutoForms;
+import cepein.mapeamento.utils.clean.application.useCase.UseCaseRequest;
 
-public class AtualizarProdutoUseCase {
+public class AtualizarProdutoUseCase implements UseCaseRequest<ProdutoForms> {
 
-    private ProdutoGateway produtoGateway;
+    private final ProdutoGateway produtoGateway;
      public AtualizarProdutoUseCase(ProdutoGateway produtoGateway){
          this.produtoGateway = produtoGateway;
      }
-     public void atualizarProduto(Produto produto){
-         this.produtoGateway.salvarProduto(produto);
+     @Override
+     public void execute(ProdutoForms produtoForms ){
+         ProdutoQuery produtoQuery = this.produtoGateway.buscar(produtoForms.getId());
+         this.produtoGateway.salvar(produtoForms.converter(produtoQuery));
      }
 }
