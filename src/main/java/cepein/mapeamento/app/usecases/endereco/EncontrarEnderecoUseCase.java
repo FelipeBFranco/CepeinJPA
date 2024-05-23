7198acef2026459e80bc5689957f9c830df64df6
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 09:46:51 -0300

    refactor: implementar interface use case  em encontrar endereco use case

--- src/main/java/cepein/mapeamento/app/usecases/endereco/EncontrarEnderecoUseCase.java
+++ src/main/java/cepein/mapeamento/app/usecases/endereco/EncontrarEnderecoUseCase.java
@@ -1,14 +1,18 @@
 package cepein.mapeamento.app.usecases.endereco;
 
-import cepein.mapeamento.acore.domain.models.Endereco;
+import cepein.mapeamento.acore.domain.models.endereco.EnderecoQuery;
 import cepein.mapeamento.app.gateways.EnderecoGateway;
+import cepein.mapeamento.utils.clean.application.useCase.UseCase;
+import cepein.mapeamento.utils.clean.application.useCase.UseCaseResponse;
 
-public class EncontrarEnderecoUseCase {
-    private EnderecoGateway enderecoGateway;
+public class EncontrarEnderecoUseCase implements UseCase<Long,EnderecoQuery> {
+    private final EnderecoGateway enderecoGateway;
     public EncontrarEnderecoUseCase(EnderecoGateway enderecoGateway){
         this.enderecoGateway = enderecoGateway;
     }
-    public Endereco encontrarEndereco(Long id){
-        return this.enderecoGateway.encontrarEnderecoPorId(id);
+    @Override
+    public EnderecoQuery execute(Long id){
+        return this.enderecoGateway.buscar(id);
     }
+
 }
