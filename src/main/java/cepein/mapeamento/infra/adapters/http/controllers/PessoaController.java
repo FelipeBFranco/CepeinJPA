4f2f21e9f65114e4d0a40f9cd8e24e368dab69a3
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 12:24:28 -0300

    refactor: renomear as chamadas dos use cases no controller de pessoa

--- src/main/java/cepein/mapeamento/infra/adapters/http/controllers/PessoaController.java
+++ src/main/java/cepein/mapeamento/infra/adapters/http/controllers/PessoaController.java
@@ -1,11 +1,14 @@
 package cepein.mapeamento.infra.adapters.http.controllers;
 
-import cepein.mapeamento.app.gateways.PessoaGatway;
+import cepein.mapeamento.app.gateways.*;
 import cepein.mapeamento.app.usecases.pessoa.*;
 import cepein.mapeamento.infra.adapters.http.dtos.PessoaDto;
 import cepein.mapeamento.infra.adapters.http.forms.PessoaForms;
+import cepein.mapeamento.infra.adapters.http.forms.PessoaPedidoForms;
+import cepein.mapeamento.infra.adapters.http.forms.PessoaProdutoForms;
 import cepein.mapeamento.infra.adapters.http.viewmodels.PessoaViewModel;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +20,33 @@ import java.util.stream.Collectors;
 @RequestMapping("/pessoa")
 public class PessoaController {
     private final PessoaGatway pessoagatway;
-
-    public PessoaController(PessoaGatway pessoagatway) {
+    private  final ProdutoGateway produtoGateway;
+    private  final PedidoGateway pedidoGateway;
+    private final PessoaProdutoGateway pessoaProdutoGateway ;
+    private  final PessoaPedidoGateway pessoaPedidoGateway ;
+    @Autowired
+    public PessoaController(PessoaGatway pessoagatway, ProdutoGateway produtoGateway, PedidoGateway pedidoGateway, PessoaProdutoGateway pessoaProdutoGateway, PessoaPedidoGateway pessoaPedidoGateway) {
         this.pessoagatway = pessoagatway;
+        this.produtoGateway = produtoGateway;
+        this.pedidoGateway = pedidoGateway;
+        this.pessoaProdutoGateway = pessoaProdutoGateway;
+        this.pessoaPedidoGateway = pessoaPedidoGateway;
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<PessoaDto> procurarPessoa(@PathVariable Long id){
+
+        EncontrarPessoaUseCase encontrarPessoaUseCase = new EncontrarPessoaUseCase(this.pessoagatway);
+
+        PessoaDto pessoaDto = PessoaViewModel.toDto(encontrarPessoaUseCase.execute(id));
+        return ResponseEntity.ok(pessoaDto);
     }
 
     @GetMapping("/listar")
     public ResponseEntity<List<PessoaDto>> listarPessoas(){
 
         EncontrarListaDePessoaUseCase encontrarListaDePessoaUseCase = new EncontrarListaDePessoaUseCase(this.pessoagatway);
-        List<PessoaDto> pessoaDtoList = encontrarListaDePessoaUseCase.encontrarListaPessoa()
+        List<PessoaDto> pessoaDtoList = encontrarListaDePessoaUseCase.execute()
                 .stream()
                 .map(PessoaViewModel::toDto)
                 .collect(Collectors.toList());
@@ -34,32 +54,39 @@ public class PessoaController {
         return ResponseEntity.ok(pessoaDtoList);
     }
 
-    @GetMapping("/procurar-por-id/{id}")
-    public ResponseEntity<PessoaDto> procurarPessoa(@PathVariable Long id){
 
-        EncontrarPessoaUseCase encontrarPessoaUseCase = new EncontrarPessoaUseCase(this.pessoagatway);
-
-        PessoaDto pessoaDto = PessoaViewModel.toDto(encontrarPessoaUseCase.encontrarPessoa(id));
-        return ResponseEntity.ok(pessoaDto);
-    }
-
-    @PostMapping("/cadastrar-pessoa-produto")
-    public ResponseEntity<HttpStatus> cadastrarPessoaComProduto(@RequestBody @Valid PessoaForms pessoaForms){
+    @PostMapping("/cadastrar")
+    public ResponseEntity<HttpStatus> cadastrarPessoa(@RequestBody @Valid PessoaForms pessoaForms){
 
         CadastrarPessoaUseCase cadastrarPessoaUseCase = new CadastrarPessoaUseCase(this.pessoagatway);
 
-        cadastrarPessoaUseCase.criarPessoa(pessoaForms.converter());
+        cadastrarPessoaUseCase.execute(pessoaForms);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+    @PostMapping("/cadastrar-pessoa-produto/{idPessoa}")
+    public ResponseEntity<HttpStatus> cadastrarPessoaProduto(@RequestBody PessoaProdutoForms pessoaProdutoForms){
 
-    @PutMapping("/atualizar/{id}")
-    public ResponseEntity<HttpStatus> atualizarPessoaExistente(@RequestBody PessoaForms pessoaForms, @PathVariable Long id){
+        CadastrarPessoaProdutoUseCase cadastrarPessoaProdutoUseCase =
+                new CadastrarPessoaProdutoUseCase(this.pessoagatway,this.produtoGateway, this.pessoaProdutoGateway);
+        cadastrarPessoaProdutoUseCase.execute(pessoaProdutoForms);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping("/cadastrar-pessoa-pedido")
+    public ResponseEntity<HttpStatus> cadastrarPessoaPedido(@RequestBody PessoaPedidoForms pessoaPedidoForms){
 
-        EncontrarPessoaUseCase encontrarPessoaUseCase = new EncontrarPessoaUseCase(this.pessoagatway);
+        CadastrarPessoaPedidoUseCase cadastrarPessoaPedidoUseCase =
+                new CadastrarPessoaPedidoUseCase(this.pessoagatway,this.pedidoGateway, this.pessoaPedidoGateway);
+
+        cadastrarPessoaPedidoUseCase.execute(pessoaPedidoForms);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping("/atualizar")
+    public ResponseEntity<HttpStatus> atualizarPessoaExistente(@RequestBody @Valid PessoaForms pessoaForms){
+
         AtualizarPessoaUseCase atualizarPessoaUseCase = new AtualizarPessoaUseCase(this.pessoagatway);
 
-        atualizarPessoaUseCase.atualizarPessoa(pessoaForms.converter(encontrarPessoaUseCase.encontrarPessoa(id)));
+        atualizarPessoaUseCase.execute(pessoaForms);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
@@ -69,7 +96,7 @@ public class PessoaController {
 
         DeletarPessoaUseCase deletarPessoaUseCase = new DeletarPessoaUseCase(this.pessoagatway);
 
-        deletarPessoaUseCase.deletarPorId(id);
+        deletarPessoaUseCase.execute(id);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
