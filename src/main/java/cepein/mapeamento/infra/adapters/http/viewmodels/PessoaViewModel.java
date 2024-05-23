8f7769ec6cacda44c95f0d251d1f91d2711fe99d
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 12:30:16 -0300

    refactor: alterar o objeto de entrada  do método toDto de pessoa view model

--- src/main/java/cepein/mapeamento/infra/adapters/http/viewmodels/PessoaViewModel.java
+++ src/main/java/cepein/mapeamento/infra/adapters/http/viewmodels/PessoaViewModel.java
@@ -1,6 +1,6 @@
 package cepein.mapeamento.infra.adapters.http.viewmodels;
 
-import cepein.mapeamento.acore.domain.models.*;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaQuery;
 import cepein.mapeamento.infra.adapters.http.dtos.*;
 
 import java.util.List;
@@ -9,54 +9,54 @@ import java.util.stream.Collectors;
 
 public class PessoaViewModel {
 
-    public static List<PessoaDto> toDto (List<Pessoa> pessoaList){
-        return  pessoaList
+    public static List<PessoaDto> toDto (List<PessoaQuery> pessoaQueryList){
+        return  pessoaQueryList
                 .stream()
                 .map(PessoaViewModel::toDto)
                 .collect(Collectors.toList());
     }
-    public static PessoaDto toDto(Pessoa pessoa){
+    public static PessoaDto toDto(PessoaQuery pessoaQuery){
         return  PessoaDto.builder()
-                .id(pessoa.getId())
-                .uuid(pessoa.getUuid())
-                .nome(pessoa.getNome())
+                .id(pessoaQuery.getId())
+                .uuid(pessoaQuery.getUuid())
+                .nome(pessoaQuery.getNome())
 
-                .enderecoPorId(Objects.isNull(pessoa.getEnderecoPorId())?
-                        null : EnderecoViewModel.toDto(pessoa.getEnderecoPorId()))
-                .enderecoPorUuid(Objects.isNull(pessoa.getEnderecoPorUuid())?
-                        null : EnderecoViewModel.toDto(pessoa.getEnderecoPorId()))
+                .enderecoPorId(Objects.isNull(pessoaQuery.getEnderecoQueryPorId())?
+                        null : EnderecoViewModel.toDto(pessoaQuery.getEnderecoQueryPorId()))
+                .enderecoPorUuid(Objects.isNull(pessoaQuery.getEnderecoQueryPorUuid())?
+                        null : EnderecoViewModel.toDto(pessoaQuery.getEnderecoQueryPorId()))
 
-                .cursoListPorId(Objects.isNull(pessoa.getCursoPorId())? null : pessoa.getCursoPorId()
+                .cursoListPorId(Objects.isNull(pessoaQuery.getCursoPorId())? null : pessoaQuery.getCursoPorId()
                                 .stream()
                                 .map(CursoViewModel::toDto)
                                 .collect(Collectors.toList()))
-                .cursoListPorUuid(Objects.isNull(pessoa.getCursoPorUuid())? null : pessoa.getCursoPorUuid()
+                .cursoListPorUuid(Objects.isNull(pessoaQuery.getCursoPorUuid())? null : pessoaQuery.getCursoPorUuid()
                         .stream()
                         .map(CursoViewModel::toDto)
                         .collect(Collectors.toList()))
 
 
-                .produtoListComJoinTable(Objects.isNull(pessoa.getProdutoListComJoinTable())?
-                        null : pessoa.getProdutoListComJoinTable()
+                .produtoListComJoinTable(Objects.isNull(pessoaQuery.getProdutoQueryListComJoinTable())?
+                        null : pessoaQuery.getProdutoQueryListComJoinTable()
                                 .stream()
                                 .map(ProdutoViewModel::toDto)
                                 .collect(Collectors.toList()))
 
-                .produtoListComEmbeddable(Objects.isNull(pessoa.getProdutoListComEmbeddable())?
-                        null :pessoa.getProdutoListComEmbeddable()
+                .produtoListComEmbeddable(Objects.isNull(pessoaQuery.getProdutoQueryListComEmbeddable())?
+                        null : pessoaQuery.getProdutoQueryListComEmbeddable()
                         .stream()
                         .map(ProdutoViewModel::toDto)
                         .collect(Collectors.toList()))
 
 
-                .pedidoListComJoinTable(Objects.isNull(pessoa.getPedidoListComJoinTable())?
-                        null : pessoa.getPedidoListComJoinTable()
+                .pedidoListComJoinTable(Objects.isNull(pessoaQuery.getPedidoQueryListComJoinTable())?
+                        null : pessoaQuery.getPedidoQueryListComJoinTable()
                                 .stream()
                                 .map(PedidoViewModel::toDto)
                                 .collect(Collectors.toList()))
 
-                .pedidoListComEmbeddable(Objects.isNull(pessoa.getPedidoListComEmbeddable())?
-                        null : pessoa.getPedidoListComJoinTable()
+                .pedidoListComEmbeddable(Objects.isNull(pessoaQuery.getPedidoQueryListComEmbeddable())?
+                        null : pessoaQuery.getPedidoQueryListComJoinTable()
                                 .stream()
                                 .map(PedidoViewModel::toDto)
                                 .collect(Collectors.toList()))
@@ -64,13 +64,13 @@ public class PessoaViewModel {
     }
 
 
-  /*  public List<PessoaProduto> createPessoaProduto(Pessoa pessoa, List<Produto> produtoList) {
+  /*  public List<PessoaProduto> createPessoaProduto(PessoaQuery pessoa, List<ProdutoQuery> produtoList) {
         List<PessoaProduto> pessoaProdutoList = new ArrayList<>();
         produtoList.forEach(produto -> { pessoaProdutoList.add(new PessoaProduto(new PessoaProdutoId(pessoa.getId(), produto.getId()), pessoa, produto));});
 
         return pessoaProdutoList;
     }
-    public List<PessoaProduto> createPessoaProduto(Produto produto,List<Pessoa> pessoaList) {
+    public List<PessoaProduto> createPessoaProduto(ProdutoQuery produto,List<PessoaQuery> pessoaList) {
         List<PessoaProduto> pessoaProdutoList = new ArrayList<>();
         pessoaList.forEach(pessoa -> { pessoaProdutoList.add(new PessoaProduto(new PessoaProdutoId(pessoa.getId(), produto.getId()), pessoa, produto));});
 
