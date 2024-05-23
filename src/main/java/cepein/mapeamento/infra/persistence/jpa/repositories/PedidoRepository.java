8f2580263246e7115bbc075ac6c3f2e0a5e14374
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 12:13:33 -0300

    refactor: deletar repository de pedido

--- src/main/java/cepein/mapeamento/infra/persistence/jpa/repositories/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package cepein.mapeamento.infra.persistence.jpa.repositories;
-
-import cepein.mapeamento.infra.persistence.jpa.entities.JpaPedidoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<JpaPedidoEntity, Long> {
-}
