f05645b11a40a429137ba787a2564c1a72aaea3e
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 09:47:34 -0300

    refactor: implementar interface use case em encontrar produto use case

--- src/main/java/cepein/mapeamento/app/usecases/produto/EncontrarProdutoUseCase.java
+++ src/main/java/cepein/mapeamento/app/usecases/produto/EncontrarProdutoUseCase.java
@@ -1,15 +1,16 @@
 package cepein.mapeamento.app.usecases.produto;
 
-import cepein.mapeamento.acore.domain.models.Produto;
+import cepein.mapeamento.acore.domain.models.produto.ProdutoQuery;
 import cepein.mapeamento.app.gateways.ProdutoGateway;
+import cepein.mapeamento.utils.clean.application.useCase.UseCase;
 
-public class EncontrarProdutoUseCase {
-    private ProdutoGateway produtoGateway;
+public class EncontrarProdutoUseCase implements UseCase<Long,ProdutoQuery> {
+    private final ProdutoGateway produtoGateway;
     public EncontrarProdutoUseCase(ProdutoGateway produtoGateway){
         this.produtoGateway = produtoGateway;
     }
-
-    public Produto encontrarProdutoPorId(Long id){
-        return this.produtoGateway.encontrarProdutoPorId(id);
+    @Override
+    public ProdutoQuery execute(Long id){
+        return this.produtoGateway.buscar(id);
     }
 }
