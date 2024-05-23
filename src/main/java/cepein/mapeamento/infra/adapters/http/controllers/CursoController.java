013e289383bd0ade6003000d2b59b6cdc11428d3
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 12:23:33 -0300

    refactor: renomear as chamadas dos use cases no controller de curso

--- src/main/java/cepein/mapeamento/infra/adapters/http/controllers/CursoController.java
+++ src/main/java/cepein/mapeamento/infra/adapters/http/controllers/CursoController.java
@@ -7,6 +7,7 @@ import cepein.mapeamento.app.usecases.pessoa.EncontrarPessoaUseCase;
 import cepein.mapeamento.infra.adapters.http.dtos.CursoDto;
 import cepein.mapeamento.infra.adapters.http.forms.CursoForms;
 import cepein.mapeamento.infra.adapters.http.viewmodels.CursoViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,17 +20,15 @@ import java.util.stream.Collectors;
 public class CursoController {
 
     private final CursoGateway cursoGateway;
-    private final PessoaGatway pessoaGatway;
-
-    public CursoController(CursoGateway cursoGateway, PessoaGatway pessoaGatway){
+    @Autowired
+    public CursoController(CursoGateway cursoGateway){
         this.cursoGateway = cursoGateway;
-        this.pessoaGatway = pessoaGatway;
     }
     @GetMapping("/buscar/{id}")
     public ResponseEntity<CursoDto> listarCursoPorId(@PathVariable Long id){
 
         EncontrarCursoUseCase encontrarCursoUseCase = new EncontrarCursoUseCase(this.cursoGateway);
-        CursoDto cursoDto = CursoViewModel.toDto(encontrarCursoUseCase.encontrarCurso(id));
+        CursoDto cursoDto = CursoViewModel.toDto(encontrarCursoUseCase.execute(id));
 
         return ResponseEntity.ok(cursoDto);
     }
@@ -37,36 +36,30 @@ public class CursoController {
     public ResponseEntity<List<CursoDto>> listarCursos(){
 
         EncontrarListaCursoUseCase encontrarListaCursoUseCase = new EncontrarListaCursoUseCase(this.cursoGateway);
-        List<CursoDto> cursoDtoList = encontrarListaCursoUseCase.encontrarListaCurso()
+        List<CursoDto> cursoDtoList = encontrarListaCursoUseCase.execute()
                 .stream()
                 .map(CursoViewModel::toDto)
                 .collect(Collectors.toList());
-
         return ResponseEntity.ok(cursoDtoList);
     }
 
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<HttpStatus> cadastraEndereco(@RequestBody CursoForms cursoForms){
+    public ResponseEntity<HttpStatus> cadastraCurso(@RequestBody CursoForms cursoForms){
 
         CadastrarCursoUseCase cadastrarCursoUseCase = new CadastrarCursoUseCase(this.cursoGateway);
-        EncontrarPessoaUseCase encontrarPessoaUseCase =new EncontrarPessoaUseCase(this.pessoaGatway);
 
-        cadastrarCursoUseCase.criarCurso(cursoForms.converter(encontrarPessoaUseCase.encontrarPessoa(cursoForms.getPessoaId())));
+        cadastrarCursoUseCase.execute(cursoForms);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PutMapping ("/atualizar/{id}")//no forms o id
-    public ResponseEntity<HttpStatus> alterarCurso(@PathVariable Long id,@RequestBody CursoForms cursoForms){
+    @PutMapping ("/atualizar")
+    public ResponseEntity<HttpStatus> alterarCurso(@RequestBody CursoForms cursoForms){
 
-        EncontrarCursoUseCase encontrarCursoUseCase = new EncontrarCursoUseCase(this.cursoGateway);
         AtualizarCursoUseCase atualizarCursoUseCase = new AtualizarCursoUseCase(this.cursoGateway);
-        EncontrarPessoaUseCase encontrarPessoaUseCase =new EncontrarPessoaUseCase(this.pessoaGatway);
 
-        atualizarCursoUseCase.atualizarCurso(cursoForms.converter(
-                encontrarCursoUseCase.encontrarCurso(id),
-                encontrarPessoaUseCase.encontrarPessoa(cursoForms.getPessoaId())));
+        atualizarCursoUseCase.execute(cursoForms);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
@@ -75,7 +68,7 @@ public class CursoController {
 
         DeletarCursoUseCase deletarCursoUseCase = new DeletarCursoUseCase(this.cursoGateway);
 
-        deletarCursoUseCase.deletarCurso(id);
+        deletarCursoUseCase.execute(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
