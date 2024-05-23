8d644a8f85068ccc979791435a4f86b904706958
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 10:10:37 -0300

    refactor: extender interface gateway em pessoa gateway

--- src/main/java/cepein/mapeamento/app/gateways/PessoaGatway.java
+++ src/main/java/cepein/mapeamento/app/gateways/PessoaGatway.java
@@ -1,13 +1,15 @@
 package cepein.mapeamento.app.gateways;
 
-import cepein.mapeamento.acore.domain.models.Pessoa;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaCommand;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaPedidoCommand;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaProdutoCommand;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaQuery;
+import cepein.mapeamento.utils.clean.application.gateway.StorageGateway;
 
 import java.util.List;
 
-public interface PessoaGatway {
+public interface PessoaGatway extends StorageGateway<PessoaCommand,PessoaQuery,Long> {
+    PessoaQuery buscar(String uuid);
+    List<PessoaQuery> encontrarTodasAsPessoa();
 
-    Pessoa encontrarPessoaPorId(Long id);
-    List<Pessoa> encontrarTodasAsPessoa();
-    void salvarPessoa(Pessoa pessoa);
-    void deletarPessoaPorId(Long id);
 }
