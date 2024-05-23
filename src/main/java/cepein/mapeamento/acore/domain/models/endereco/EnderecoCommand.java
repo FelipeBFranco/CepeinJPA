6ad582eb96c50b1fabd0f6b1c03c4639ad7ff97b
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-23 10:43:40 -0300

    feat: criar model command de endereco

--- src/main/java/cepein/mapeamento/acore/domain/models/endereco/EnderecoCommand.java
+++ src/main/java/cepein/mapeamento/acore/domain/models/endereco/EnderecoCommand.java
@@ -1,5 +1,6 @@
-package cepein.mapeamento.acore.domain.models;
+package cepein.mapeamento.acore.domain.models.endereco;
 
+import cepein.mapeamento.acore.domain.validators.Tamanho;
 import lombok.*;
 
 @Getter
@@ -7,20 +8,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Endereco {
+public class EnderecoCommand {
 
     private Long id_endereco;
 
     private String uuid;
 
+    @Tamanho(max = "100",min = "5")
     private String rua;
 
     private String cep;
 
+    @Tamanho(max = "100",min = "5")
     private String cidade;
 
     private String estado;
 
-    private Pessoa pessoaPorId;
-    private Pessoa pessoaPorUuid;
 }
