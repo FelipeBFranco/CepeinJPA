294c328d577ab152cc43007025747e2e9a6ea8fd
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-05-24 08:44:41 -0300

    refactor: alterar o objeto de entrada e saida dos métodos de produto mapper

--- src/main/java/cepein/mapeamento/infra/persistence/jpa/mapper/JpaProdutoMapper.java
+++ src/main/java/cepein/mapeamento/infra/persistence/jpa/mapper/JpaProdutoMapper.java
@@ -1,50 +1,52 @@
 package cepein.mapeamento.infra.persistence.jpa.mapper;
 
-import cepein.mapeamento.acore.domain.models.Pessoa;
-import cepein.mapeamento.acore.domain.models.Produto;
-import cepein.mapeamento.infra.persistence.jpa.entities.JpaPessoaEntity;
-import cepein.mapeamento.infra.persistence.jpa.entities.JpaPessoaProdutoEntity;
-import cepein.mapeamento.infra.persistence.jpa.entities.JpaProdutoEntity;
+import cepein.mapeamento.acore.domain.models.pessoa.PessoaQuery;
+import cepein.mapeamento.acore.domain.models.produto.ProdutoCommand;
+import cepein.mapeamento.acore.domain.models.produto.ProdutoQuery;
+import cepein.mapeamento.infra.persistence.jpa.entities.pessoa.JpaPessoaQueryEntity;
+import cepein.mapeamento.infra.persistence.jpa.entities.pessoa.pessoaProduto.JpaPessoaProdutoQueryEntity;
+import cepein.mapeamento.infra.persistence.jpa.entities.produto.JpaProdutoCommandEntity;
+import cepein.mapeamento.infra.persistence.jpa.entities.produto.JpaProdutoQueryEntity;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class JpaProdutoMapper {
-    public static JpaProdutoEntity toEntity(Produto produtoDomain){
-        return JpaProdutoEntity.builder()
-                .id(produtoDomain.getId())
-                .descricao(produtoDomain.getDescricao())
+    public static JpaProdutoCommandEntity toEntity(ProdutoCommand produtoCommand){
+        return JpaProdutoCommandEntity.builder()
+                .id(produtoCommand.getId())
+                .descricao(produtoCommand.getDescricao())
                 .build();
     }
-    public  static Produto toDomain(JpaProdutoEntity jpaProdutoEntity){
-        return Produto.builder()
-                .id(jpaProdutoEntity.getId())
-                .descricao(jpaProdutoEntity.getDescricao())
-                .pessoaListComJoinTable(JpaProdutoMapper.pessoaDeProdutoToDomain(jpaProdutoEntity.getPessoaListComJoinTable()))
-                .pessoaListComEmbeddable(JpaProdutoMapper.pessoaProdutoDeProdutoToDomain(jpaProdutoEntity.getPessoaListComEmbeddable()))
+    public  static ProdutoQuery toDomain(JpaProdutoQueryEntity jpaProdutoQueryEntity){
+        return ProdutoQuery.builder()
+                .id(jpaProdutoQueryEntity.getId())
+                .descricao(jpaProdutoQueryEntity.getDescricao())
+                .pessoaQueryListComJoinTable(JpaProdutoMapper.pessoaDeProdutoToDomain(jpaProdutoQueryEntity.getPessoaListComJoinTable()))
+                .pessoaQueryListComEmbeddable(JpaProdutoMapper.pessoaProdutoDeProdutoToDomain(jpaProdutoQueryEntity.getPessoaListComEmbeddable()))
                 .build();
     }
 
-    public static List<Pessoa> pessoaDeProdutoToDomain(List<JpaPessoaEntity> jpaPessoaEntityList){
-        return jpaPessoaEntityList
+    public static List<PessoaQuery> pessoaDeProdutoToDomain(List<JpaPessoaQueryEntity> jpaPessoaQueryEntityList){
+        return jpaPessoaQueryEntityList
                 .stream()
-                .map(jpaPessoaEntity ->Pessoa.builder()
+                .map(jpaPessoaEntity -> PessoaQuery.builder()
                         .id(jpaPessoaEntity.getId())
                         .uuid(jpaPessoaEntity.getUuid())
                         .nome(jpaPessoaEntity.getNome())
-                        .enderecoPorId(JpaPessoaMapper.toDomain(jpaPessoaEntity.getEnderecoPorId()))
-                        .enderecoPorUuid(JpaPessoaMapper.toDomain(jpaPessoaEntity.getEnderecoPorUuid()))
+                        .enderecoQueryPorId(JpaPessoaMapper.toDomain(jpaPessoaEntity.getEnderecoPorId()))
+                        .enderecoQueryPorUuid(JpaPessoaMapper.toDomain(jpaPessoaEntity.getEnderecoPorUuid()))
                         .cursoPorId(JpaPessoaMapper.cursoDePessoaToDomain(jpaPessoaEntity.getCursoPorId()))
                         .cursoPorUuid(JpaPessoaMapper.cursoDePessoaToDomain(jpaPessoaEntity.getCursoPorUuid()))
-                        .pedidoListComJoinTable(JpaPessoaMapper.pedidoDePessoaToDomain(jpaPessoaEntity.getPedidoListComJoinTable()))
-                        .pedidoListComEmbeddable(JpaPessoaMapper.pessoaPedidoDePessoaToDomain(jpaPessoaEntity.getPedidoListComEmbeddable()))
+                        .pedidoQueryListComJoinTable(JpaPessoaMapper.pedidoDePessoaToDomain(jpaPessoaEntity.getPedidoListComJoinTable()))
+                        .pedidoQueryListComEmbeddable(JpaPessoaMapper.pessoaPedidoDePessoaToDomain(jpaPessoaEntity.getPedidoListComEmbeddable()))
                         .build() )
                 .collect(Collectors.toList());
     }
-    public static List<Pessoa> pessoaProdutoDeProdutoToDomain(List<JpaPessoaProdutoEntity> jpaPessoaProdutoEntityEntityList){
-        List<JpaPessoaEntity> pessoaEntityList = jpaPessoaProdutoEntityEntityList
+    public static List<PessoaQuery> pessoaProdutoDeProdutoToDomain(List<JpaPessoaProdutoQueryEntity> jpaPessoaProdutoEntityQueryEntityList){
+        List<JpaPessoaQueryEntity> pessoaEntityList = jpaPessoaProdutoEntityQueryEntityList
                 .stream()
-                .map(JpaPessoaProdutoEntity::getPessoa)
+                .map(JpaPessoaProdutoQueryEntity::getPessoa)
                 .collect(Collectors.toList());
         return JpaProdutoMapper.pessoaDeProdutoToDomain(pessoaEntityList);
     }
